5ac76b5e0bd8911543c492348baf14ead24669fd
VictorColominaAnoll <victorcolominanoll@gmail.com>
2021-06-01 11:23:34 +0200

    Empty project

---
+++ src/main/java/victor/colomina/anoll/tictactoekata/TicTacToeKataApplication.java
@@ -0,0 +1,13 @@
+package victor.colomina.anoll.tictactoekata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeKataApplication.class, args);
+	}
+
+}
